ebd3a781a5dda7a0e8602e099f0c6461fce1016e
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-20 14:16:56 -0600

    passed exampleVideoData into videoList and dynamically rendered VideoListEntry, passed VideoListSpec

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,10 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {/* from exampleVideoData, we can see the structure of each video */
+      props.videos.map((video) =>
+        <VideoListEntry video={video} key={video.etag} />)}
   </div>
 );
 
