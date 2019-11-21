ace4dee2e58f7283440588d1bdd058fc7a0a621e
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-20 21:52:36 -0600

    Created a reusable API helper using Search:list endpoint, passed all the tests for searchYouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,26 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  // The $.get() method requests data from the server with an HTTP GET request.
+  // Here 'https://www.googleapis.com/youtube/v3/search' is the URL, following with the callback
+  // The template for sending a GET request to the Youtube Search:list API is: https://developers.google.com/youtube/v3/docs/search/list, we can also refer to SearchYouTubeSpec.js(line 63)
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  })
+    .done(({ items }) => {
+      console.log("items = ", items);
+      if (callback) {
+        callback(items);
+      }
+    })
+    .fail(({responseJSON}) => {
+      responseJSON.error.errors.forEach((err) =>
+        console.error(err)
+      );
+    });
 };
 
 export default searchYouTube;
